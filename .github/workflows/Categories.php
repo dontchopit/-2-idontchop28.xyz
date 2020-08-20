b674cc970b99857544ac0aecbf8cad39bb562a6a
dontchopit <69779064+dontchopit@users.noreply.github.com>
2020-08-20 18:25:56 +0100

    Categories.php

---
+++ .github/workflows/Categories.php
@@ -0,0 +1,37 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title> idontchop28.xyz </title>
+
+<header>
+  
+  
+  <body>
+  <ul>
+<nav> 
+<a href="#">
+
+<h1 style="color:blue;">welcome to idontchop28</h1>
+<p style="color:red;">idontchop.xyz is finally getting updated. Will soon be back too business! New features coming soon! Stay tuned.</p>
+
+<li><a href="login.html"> login</a></li> 
+<li><a href="topup.html">Top up</a></li>
+ <li><a href="recentpurchases.html">Recent Purchases</a></li>
+<li><a href="28clothing.html">28clothing</a></li>
+     <h4> shop</h4>
+<li><a href="accounts.html">Accounts</a></li>
+<li><a href="methods.html">Methods</a></li>
+<li><a href="bins.html">Bins</a></li>
+<li><a href="Fullz.html">Fullz</a></li>
+<li><a href="Dumpz.html">Dumpz</a></li>
+<li><a href="clickinglesson.html">Clicking Lesson</a></li>
+<li><a href="crackinglesson.html">Cracking Lesson</a></li>
+<li><a href="spam.html">Spam Services</a></li>
+<li><a href="checker.html">Checker</a></li>
+<li><a href="dox.html">Doxing Services</a></li>
+ 
+</body>
+ 
+ </header> 
+  
+  
